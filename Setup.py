0563af60f5e5382fd0add609dcb4c6ade53ea293
YisoIA <yasinislam.liu@gmail.com>
2026-03-14 01:14:27 +0600

    color correction

--- Setup.py
+++ Setup.py
@@ -27,10 +27,12 @@ def find_comparison(image):
     print("Cropping image...")
     h, w, _ = image.shape
 
+    image_float = image.astype(np.float32) / 255.0
+
     #extract landmarks
 
     # loading the image here
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_float)
     detection_result = landmarker.detect(mp_image)
             
     if detection_result.face_landmarks:
@@ -45,9 +47,8 @@ def find_comparison(image):
             polygon = np.array([[int(face[i].x * w), int(face[i].y * h)] for i in indices], dtype=np.int32)
             mask = np.zeros((h,w), dtype=np.uint8)
             cv2.fillPoly(mask, [polygon], 255)
-            pixels = image[mask > 0]
-            pixels_float = pixels.astype(np.float32) / 255.0
-            pixel_reshaped = pixels_float.reshape(1, -1, 3)
+            pixels = image_float[mask > 0]
+            pixel_reshaped = pixels.reshape(1, -1, 3)
             return cv2.cvtColor(pixel_reshaped, cv2.COLOR_RGB2LAB)[0]
 
         left_lab = get_cheek_lab(left_cheek_indices)
@@ -72,6 +73,9 @@ def find_comparison(image):
 
         # averaging the middle
         target_lab = trimmed_labs.mean(axis=0)
+        target_lab[0] = np.clip(target_lab[0] + 5.0, 0, 100)
+        target_lab[1] = target_lab[1] - 4.0
+        target_lab[2] = target_lab[2] - 8.0
 
         best_matches = find_my_match(target_lab)
 
